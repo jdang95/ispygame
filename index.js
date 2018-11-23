f72cd01e2f015a380ced3a95ec3bc22341f555de
Julia Dang <43449736+jdang95@users.noreply.github.com>
2018-11-22 21:48:22 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello words");
+
+const area1 = document.getElementById('area1');
+const area2 = document.getElementById('area2');
+const area2 = document.getElementById('area3');
+
+area1.onclick = handleOnClick;
+area2.onclick = handleOnClick;
+
+function handleOnClick(event) {
+	event.preventDefault();
+	console.log("I was clicked");
+	console.log(event);
+}
